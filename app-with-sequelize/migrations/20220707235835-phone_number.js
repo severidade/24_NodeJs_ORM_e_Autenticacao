773168479d0f3bf6c5d2a6c0d85d929acd7a0bff
severo <oliveira.severo@gmail.com>
2022-07-08 11:48:44 -0300

    commit inicial conteúdo completo

---
+++ app-with-sequelize/migrations/20220707235835-phone_number.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.addColumn('Users', 'phone_number', {
+     type: Sequelize.STRING,
+   });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'phone_number');
+  }
+};
